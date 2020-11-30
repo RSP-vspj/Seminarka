88b97a211165890318a7832d91cd92285cde1dc8
j0hnycz <72764450+j0hnycz@users.noreply.github.com>
2020-11-30 08:53:27 +0100

    layout

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,11 +5,23 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="alert")
+     */
+    public function alertAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/alert.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/homepage", name="homepage")
      */
     public function indexAction(Request $request)
     {
@@ -18,4 +30,60 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/onas", name="onas")
+     */
+    public function onasAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/onas.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/kontakt", name="kontakt")
+     */
+    public function kontaktAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/kontakt.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/pokyny", name="pokyny")
+     */
+    public function pokynyAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/pokyny.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/sablona", name="sablona")
+     **/
+    public function sablonaAction(){
+
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../web/files/';
+        $filename = "Šablona LOGOS POLYTECHNIKOS.docx";
+
+        return new BinaryFileResponse($publicResourcesFolderPath.$filename);
+    }
+
+    /**
+     * @Route("/recenzni_rizeni", name="recenzni_rizeni")
+     **/
+    public function recenzni_rizeniAction(){
+
+        $publicResourcesFolderPath = $this->get('kernel')->getRootDir() . '/../web/files/';
+        $filename = "Recenzní řízení časopisu LOGOS POLYTECHNIKOS.pdf";
+
+        return new BinaryFileResponse($publicResourcesFolderPath.$filename);
+    }
+
 }
